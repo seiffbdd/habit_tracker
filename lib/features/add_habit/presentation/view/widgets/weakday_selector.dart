9e2215efd33c60cc2f6b5be3f7820a966ac81921
Seif Elnawawy <elnawawyseif@gmail.com>
2025-05-27 02:53:01 +0300

    finished weakday_selector UI

--- lib/features/add_habit/presentation/view/widgets/weakday_selector.dart
+++ lib/features/add_habit/presentation/view/widgets/weakday_selector.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:habit_tracker/core/constants/ui_constants.dart';
+import 'package:habit_tracker/core/theme/app_colors.dart';
 
 class WeekdaySelector extends StatefulWidget {
   const WeekdaySelector({super.key});
@@ -15,65 +18,61 @@ class _WeekdaySelectorState extends State<WeekdaySelector> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
-        color: const Color(0xFF2C2F38), // Dark background
-        borderRadius: BorderRadius.circular(12),
+        color: AppColors.lightGrey,
+        borderRadius: BorderRadius.circular(4.r),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          /// Title row with checkmark
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Text(
-                'Specific days in week',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              Icon(Icons.check_circle, color: Colors.green),
-            ],
+          /// Days of the week title
+          Text(
+            'Everyday',
+            style: Theme.of(
+              context,
+            ).textTheme.displaySmall!.copyWith(color: AppColors.grey),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
           ),
-          const SizedBox(height: 8),
+          height16,
 
-          /// Subtitle
-          const Text('Everyday', style: TextStyle(color: Colors.grey)),
-          const SizedBox(height: 12),
-
-          /// Days selector
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(7, (index) {
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedDays[index] = !selectedDays[index];
-                  });
-                },
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color:
-                        selectedDays[index]
-                            ? const Color(0xFF3E82FF)
-                            : Colors.grey.shade700,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    dayLabels[index],
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              );
-            }),
-          ),
+          _daysSelector(),
         ],
       ),
+    );
+  }
+
+  Row _daysSelector() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(7, (index) {
+        return GestureDetector(
+          onTap: () {
+            // i will handle it with cubit
+            setState(() {
+              selectedDays[index] = !selectedDays[index];
+            });
+          },
+          child: Container(
+            width: 40.h,
+            height: 40.h,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color:
+                  selectedDays[index] ? AppColors.darkOrange : AppColors.grey,
+              borderRadius: BorderRadius.circular(8.r),
+            ),
+            child: Text(
+              dayLabels[index],
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
