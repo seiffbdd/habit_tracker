b6ac815680c2409f00ced15469333776288b285e
Seif Elnawawy <elnawawyseif@gmail.com>
2025-05-27 02:36:05 +0300

    created build_bottom_sheet

---
+++ lib/features/add_habit/presentation/view/widgets/build_bottom_sheet.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/core/theme/app_colors.dart';
+
+class BuildBottomSheet extends StatelessWidget {
+  const BuildBottomSheet({
+    super.key,
+    this.title,
+    this.titleText,
+    this.footer,
+    this.body,
+  });
+  final Widget? title;
+  final String? titleText;
+  final Widget? body;
+  final Widget? footer;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // title widget or text
+            Expanded(
+              child:
+                  title ??
+                  Text(
+                    titleText ?? '',
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      color: AppColors.lightGrey,
+                    ),
+                  ),
+            ),
+
+            // body of the bottom sheet
+            Expanded(flex: 6, child: body ?? SizedBox.shrink()),
+            // footer of the bottom sheet
+            if (footer != null) Expanded(child: footer!),
+          ],
+        ),
+      ),
+    );
+  }
+}
