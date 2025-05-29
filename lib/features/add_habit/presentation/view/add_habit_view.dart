55f9e91375a52ba5d1349cafb1a3bc27363482d0
Seif Elnawawy <elnawawyseif@gmail.com>
2025-05-29 23:56:04 +0300

    used the custom widgets in the add habit view

--- lib/features/add_habit/presentation/view/add_habit_view.dart
+++ lib/features/add_habit/presentation/view/add_habit_view.dart
@@ -3,10 +3,15 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:habit_tracker/core/constants/ui_constants.dart';
 import 'package:habit_tracker/core/theme/app_colors.dart';
+import 'package:habit_tracker/core/utils/components.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_bottom_sheet.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_tab_bar.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/default_number_picker.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/labeled_action_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/custom_text_form_field.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/icon_and_color_card.dart';
+import 'package:habit_tracker/core/widgets/gredient_button.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/weakday_selector.dart';
 
 class AddHabitView extends StatefulWidget {
   const AddHabitView({super.key});
@@ -34,13 +39,15 @@ class _AddHabitViewState extends State<AddHabitView>
     _tabController.dispose();
   }
 
+  int _hour = 0;
+  int _minute = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actionsPadding: EdgeInsets.only(right: 12.0.w),
-        title: Padding(
-          padding: EdgeInsets.only(left: 12.0.w),
+        actionsPadding: const EdgeInsets.only(right: 12.0),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 12.0),
           child: Text('Create New Habit'),
         ),
         automaticallyImplyLeading: false,
@@ -49,12 +56,12 @@ class _AddHabitViewState extends State<AddHabitView>
             onPressed: () {
               context.pop();
             },
-            icon: Icon(Icons.close),
+            icon: const Icon(Icons.close),
           ),
         ],
       ),
       body: Padding(
-        padding: EdgeInsets.all(12.w),
+        padding: const EdgeInsets.all(12),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -63,15 +70,12 @@ class _AddHabitViewState extends State<AddHabitView>
               height20,
               CustomTextFormField(controller: _habitNameController),
               height20,
-              IconAndColorCard(),
+              const IconAndColorCard(),
               height16,
               Text('Repeat', style: Theme.of(context).textTheme.displayMedium),
               height8,
-              LabeledActionButton(
-                labelText: 'Habit Days',
-                sublabelText: 'Everyday',
-                onPressed: () {},
-              ),
+              const WeekdaySelector(),
+
               height16,
               Text(
                 'Do It At',
@@ -81,6 +85,45 @@ class _AddHabitViewState extends State<AddHabitView>
               LabeledActionButton(
                 labelText: 'Habit Time',
                 sublabelText: '08:00',
+                onPressed: () async {
+                  await Components.showDefaultBottomSheet(
+                    context,
+                    child: StatefulBuilder(
+                      // i will handle it with cubit
+                      builder: (context, setState) {
+                        return BuildBottomSheet(
+                          titleText: 'Choose the time',
+                          body: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              DefaultNumberPicker(
+                                minValue: 0,
+                                maxValue: 23,
+                                value: _hour,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _hour = value;
+                                  });
+                                },
+                              ),
+                              DefaultNumberPicker(
+                                minValue: 0,
+                                maxValue: 59,
+                                value: _minute,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _minute = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          footer: GredientButton(text: 'Save'),
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
               height16,
               Text(
@@ -88,13 +131,13 @@ class _AddHabitViewState extends State<AddHabitView>
                 style: Theme.of(context).textTheme.displayMedium,
               ),
               height8,
-              LabeledActionButton(labelText: 'Goal', sublabelText: 'off'),
+              const LabeledActionButton(labelText: 'Goal', sublabelText: 'off'),
               height16,
               Text(
                 'Remineder',
                 style: Theme.of(context).textTheme.displayMedium,
               ),
-              LabeledActionButton(
+              const LabeledActionButton(
                 labelText: 'Reminder for Habit',
                 suffixIcon: Icons.radio_button_checked,
                 iconColor: AppColors.darkOrange,
@@ -107,11 +150,11 @@ class _AddHabitViewState extends State<AddHabitView>
                 height: 200.h,
                 child: TabBarView(
                   controller: _tabController,
-                  physics: NeverScrollableScrollPhysics(),
+                  physics: const NeverScrollableScrollPhysics(),
                   children: [
-                    Center(child: Text('Off')),
-                    Center(child: Text('Date')),
-                    Center(child: Text('Days')),
+                    const Center(child: Text('Off')),
+                    const Center(child: Text('Date')),
+                    const Center(child: Text('Days')),
                   ],
                 ),
               ),
