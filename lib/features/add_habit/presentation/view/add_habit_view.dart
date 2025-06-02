8ac1ea5bb15865c2adf5ee4362422b0b717b80e7
Seif Elnawawy <elnawawyseif@gmail.com>
2025-06-02 08:56:43 +0300

    used the custom widgets in AddHabitView

--- lib/features/add_habit/presentation/view/add_habit_view.dart
+++ lib/features/add_habit/presentation/view/add_habit_view.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:habit_tracker/core/constants/ui_constants.dart';
 import 'package:habit_tracker/core/theme/app_colors.dart';
-import 'package:habit_tracker/core/utils/components.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_bottom_sheet.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_goal_button.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_reminder_switch.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_tab_bar.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view/widgets/default_number_picker.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/goal_type_card.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view/widgets/labeled_action_button.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/habit_time_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/custom_text_form_field.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/icon_and_color_card.dart';
-import 'package:habit_tracker/core/widgets/gredient_button.dart';
+import 'package:habit_tracker/features/add_habit/presentation/view/widgets/reminder_time_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/weakday_selector.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view_model/goal_type/goal_type_cubit.dart';
 
 class AddHabitView extends StatefulWidget {
   const AddHabitView({super.key});
@@ -42,11 +39,8 @@ class _AddHabitViewState extends State<AddHabitView>
     _tabController.dispose();
   }
 
-  int _hour = 0;
-  int _minute = 0;
   @override
   Widget build(BuildContext context) {
-    GoalTypeCubit goalTypeCubit = context.read<GoalTypeCubit>();
     return Scaffold(
       appBar: AppBar(
         actionsPadding: const EdgeInsets.only(right: 12.0),
@@ -86,81 +80,24 @@ class _AddHabitViewState extends State<AddHabitView>
                 style: Theme.of(context).textTheme.displayMedium,
               ),
               height8,
-              LabeledActionButton(
-                labelText: 'Habit Time',
-                sublabelText: '08:00',
-                onPressed: () async {
-                  await Components.showDefaultBottomSheet(
-                    context,
-                    child: StatefulBuilder(
-                      // i will handle it with cubit
-                      builder: (context, setState) {
-                        return BuildBottomSheet(
-                          titleText: 'Choose the time',
-                          body: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              DefaultNumberPicker(
-                                minValue: 0,
-                                maxValue: 23,
-                                value: _hour,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _hour = value;
-                                  });
-                                },
-                              ),
-                              DefaultNumberPicker(
-                                minValue: 0,
-                                maxValue: 59,
-                                value: _minute,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _minute = value;
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                          footer: GredientButton(text: 'Save'),
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
+              const HabitTimeButton(),
               height16,
               Text(
                 'Daily Goal',
                 style: Theme.of(context).textTheme.displayMedium,
               ),
               height8,
-              BlocBuilder<GoalTypeCubit, GoalTypeState>(
-                buildWhen:
-                    (previous, current) =>
-                        current is GoalTypeChanged ||
-                        previous is GoalTypeChanged,
-                builder: (context, state) {
-                  return LabeledActionButton(
-                    labelText: 'Goal',
-                    sublabelText: goalTypeCubit.goalType.name,
-                    onPressed: () {
-                      goalTypeCubit.changeGoalType();
-                    },
-                  );
-                },
-              ),
-              GoalTypeCard(),
+              const BuildGoalButton(),
+              const GoalTypeCard(),
               height16,
               Text(
                 'Remineder',
                 style: Theme.of(context).textTheme.displayMedium,
               ),
-              const LabeledActionButton(
-                labelText: 'Reminder for Habit',
-                suffixIcon: Icons.radio_button_checked,
-                iconColor: AppColors.darkOrange,
-              ),
+              height8,
+
+              const BuildReminderSwitch(),
+              const ReminderTimeButton(),
               height16,
               Text('Ends On', style: Theme.of(context).textTheme.displayMedium),
               height8,
