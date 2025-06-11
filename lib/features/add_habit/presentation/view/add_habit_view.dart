2ad4b4a36aa79db325a1337b393cdd2b03afed73
Seif Elnawawy <elnawawyseif@gmail.com>
2025-06-11 21:37:23 +0300

    finished add habit view UI

--- lib/features/add_habit/presentation/view/add_habit_view.dart
+++ lib/features/add_habit/presentation/view/add_habit_view.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:habit_tracker/core/constants/ui_constants.dart';
 import 'package:habit_tracker/core/theme/app_colors.dart';
-import 'package:habit_tracker/core/utils/components.dart';
 import 'package:habit_tracker/core/widgets/gredient_button.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_bottom_sheet.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_date_picker.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_goal_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_reminder_switch.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/build_tab_bar.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view/widgets/default_number_picker.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/ends_on_after_x_days.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/goal_type_card.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/habit_time_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/custom_text_form_field.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/icon_and_color_card.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view/widgets/labeled_action_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/reminder_time_button.dart';
 import 'package:habit_tracker/features/add_habit/presentation/view/widgets/weakday_selector.dart';
-import 'package:habit_tracker/features/add_habit/presentation/view_model/ends_on_cubit/ends_on_cubit.dart';
 
 class AddHabitView extends StatefulWidget {
   const AddHabitView({super.key});
@@ -69,69 +63,87 @@ class _AddHabitViewState extends State<AddHabitView>
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Divider(color: AppColors.lightGrey, height: 1.h),
-              height20,
-              CustomTextFormField(controller: _habitNameController),
-              height20,
-              const IconAndColorCard(),
-              height16,
-              Text('Repeat', style: Theme.of(context).textTheme.displayMedium),
-              height8,
-              const WeekdaySelector(),
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Divider(color: AppColors.lightGrey, height: 1.h),
+                  height20,
+                  CustomTextFormField(controller: _habitNameController),
+                  height20,
+                  const IconAndColorCard(),
+                  height16,
+                  Text(
+                    'Repeat',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  height8,
+                  const WeekdaySelector(),
 
-              height16,
-              Text(
-                'Do It At',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-              height8,
-              const HabitTimeButton(),
-              height16,
-              Text(
-                'Daily Goal',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-              height8,
-              const BuildGoalButton(),
-              const GoalTypeCard(),
-              height16,
-              Text(
-                'Remineder',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-              height8,
+                  height16,
+                  Text(
+                    'Do It At',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  height8,
+                  const HabitTimeButton(),
+                  height16,
+                  Text(
+                    'Daily Goal',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  height8,
+                  const BuildGoalButton(),
+                  const GoalTypeCard(),
+                  height16,
+                  Text(
+                    'Remineder',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  height8,
 
-              const BuildReminderSwitch(),
-              const ReminderTimeButton(),
-              height16,
-              Text('Ends On', style: Theme.of(context).textTheme.displayMedium),
-              height8,
-              BuildTabBar(tabController: _tabController, tabs: _endsOntabs),
-              SizedBox(
-                height: 80.h,
-                child: TabBarView(
-                  controller: _tabController,
+                  const BuildReminderSwitch(),
+                  const ReminderTimeButton(),
+                  height16,
+                  Text(
+                    'Ends On',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  height8,
+                  BuildTabBar(tabController: _tabController, tabs: _endsOntabs),
+                  SizedBox(
+                    height: 80.h,
+                    child: TabBarView(
+                      controller: _tabController,
 
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    const SizedBox.shrink(),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 8.0),
-                      child: BuildDatePicker(),
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        const SizedBox.shrink(),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: BuildDatePicker(),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: EndsOnAfterXDays(),
+                        ),
+                      ],
                     ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 8.0),
-                      child: EndsOnAfterXDays(),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 60.h),
+                ],
               ),
-            ],
-          ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: 60.h,
+                child: GredientButton(text: 'Create New', onPressed: () {}),
+              ),
+            ),
+          ],
         ),
       ),
     );
